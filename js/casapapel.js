c1b815e66ebfb41e7b3509c389ac846502ce214f
Alejandro <element3999@gmail.com>
2025-04-22 22:08:13 -0300

    continuación

---
+++ js/casapapel.js
@@ -0,0 +1,4 @@
+document.getElementById("boton-hamburguesa").addEventListener("click", function () {
+    const menu = document.getElementById("menu-hamburguesa");
+    menu.classList.toggle("oculto");
+  });
